b3f1abe79f5bd41f1d892c6890dbc791a0b120fe
Nils Maier <maierman@web.de>
2011-01-19 17:12:05 +0100

    Disable xul cache as well

--- bootstrap.js
+++ bootstrap.js
@@ -1,9 +1,20 @@
+const PREF_XULCACHE = "nglayout.debug.disable_xul_cache";
+
 Components.utils.import("resource://gre/modules/Services.jsm");
 
 function install() {}
 function uninstall() {}
-function shutdown() {}
 
 function startup() {
 	Services.appinfo.invalidateCachesOnRestart();
+	Services.prefs.setBoolPref(PREF_XULCACHE, true);
+	Components.utils.reportError("Purge Caches: going to purge caches when you restart!");
+}
+function shutdown() {
+	try {
+		Services.prefs.clearUserPref(PREF_XULCACHE);
+	}
+	catch (ex) {
+		// no op
+	}
 }
